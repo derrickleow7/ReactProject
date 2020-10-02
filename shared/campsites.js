8ffbcf6ee2ea1025f801c87b5f7d755eca1096a7
Derrick Leow <derrickleow7@gmail.com>
2020-10-02 12:19:57 -0700

    Commit

---
+++ shared/campsites.js
@@ -0,0 +1,34 @@
+export const CAMPSITES = [
+    {
+        id: 0,
+        name: "Inside Singapore Airlines",
+        image: "/assets/images/react-lake.jpg",
+        elevation: 1233,
+        featured: false,
+        description: "Learn more about what goes on behind the scenes at the SIA Training Centre. For two weekends only, we are opening our doors to give you an inside look into our world with guided tours, activities and curated experiences for all ages.​"
+    },
+    {
+        id: 1,
+        name:"Restaurant A380@Changi",
+        image: "/assets/images/chrome-river.jpg",
+        elevation: 877,
+        featured: false,
+        description: "Join us for a memorable dining experience on board the world's largest passenger aircraft. Savour a signature Singapore Airlines dish. Or enjoy favourites from our Peranakan menu, specially designed by acclaimed chef Shermay Lee."
+    },
+    {
+        id: 2,
+        name:"SIA@Home",
+        image: "/assets/images/breadcrumb-trail.jpg",
+        elevation: 2901,
+        featured: false,
+        description: "Enjoy a taste of Singapore Airlines, delivered to your home. Order a home-dining experience for two created by our world-renowned International Culinary Panel chefs, paired with your choice of wine or champagne."
+    },
+    {
+        id: 3,
+        name:"Flight simulator experience",
+        image: "/assets/images/redux-woods.jpg",
+        elevation: 42,
+        featured: true,
+        description: "Experience the training that goes behind being a pilot with a flying instructor. Take command and go from take-off to landing in the very same flight simulator Singapore Airlines pilots use for training."
+    }
+];
